61bbe78a89a48b163bf5eef533d4e58b729c919b
kduesler <kyle.duesler.09@gmail.com>
2025-03-13 15:39:53 -0500

    recomendations box and text

--- me.js
+++ me.js
@@ -1,3 +1,21 @@
+//Page JS
+const recInput = document.getElementById("recInput");
+const recButton = document.getElementById("recButton");
+const outputDiv = document.getElementById("output");
+
+recButton.addEventListener("click", function submitRec() {
+  console.log(recInput.value);
+  const text = recInput.value;
+
+  if (recInput) {
+    const newParagraph = document.createElement("p");
+    newParagraph.textContent = text;
+    outputDiv.appendChild(newParagraph);
+    recInput.value = "";
+    recInput.placeholder = "Tell me what you like about me here:";
+  }
+});
+
 //Rock Paper Scissors JS
 let myChoice = null;
 
@@ -49,33 +67,31 @@ function compareChoices(userChoice) {
 }
 
 document.getElementById("reset").addEventListener("click", function () {
-    document.getElementById("my-result").innerHTML = "";
-    document.getElementById("comp-result").innerHTML = "";
-    document.getElementById("game-results").innerHTML = "";
-})
-
-
+  document.getElementById("my-result").innerHTML = "";
+  document.getElementById("comp-result").innerHTML = "";
+  document.getElementById("game-results").innerHTML = "";
+});
 
 // Temperature Converter JS
 let inputTempF = document.getElementById("inputF");
 
 document.getElementById("FtoCbutton").addEventListener("click", function () {
   let newFtemp = newFtoC(inputTempF.value);
-   document.getElementById("far").innerHTML= inputTempF.value + " in Fahrenheit is " + newFtemp + " in Celsius.";
+  document.getElementById("far").innerHTML =
+    inputTempF.value + " in Fahrenheit is " + newFtemp + " in Celsius.";
 });
 //Fahrenheit to Celsius function
 let newFtoC = function FtoC(tempF) {
-  return (((tempF - 32) / 9) * 5);
-}
-
+  return ((tempF - 32) / 9) * 5;
+};
 
 let inputTempC = document.getElementById("inputC");
-document.getElementById("CtoFbutton").addEventListener("click", function(){
-    let newCtemp = newCtoF(inputTempC.value);
-    document.getElementById("cel").innerHTML= inputTempC.value + " in Celsius is " + newCtemp + " in Fahrenheit.";
+document.getElementById("CtoFbutton").addEventListener("click", function () {
+  let newCtemp = newCtoF(inputTempC.value);
+  document.getElementById("cel").innerHTML =
+    inputTempC.value + " in Celsius is " + newCtemp + " in Fahrenheit.";
 });
 //Celsius to Fahrenheit function
 let newCtoF = function CtoF(tempC) {
-  return ((tempC / 5) * 9 + 32);
-}
-
+  return (tempC / 5) * 9 + 32;
+};
